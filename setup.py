44bbbe5b67c1c456ce5b446564be5d8970685aa6
jeff.wade <jeff.s.wade@rtx.com>
2025-08-07 07:48:54 -0700

    Update package dependencies

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
-        'geopandas~=1.0',
-        'pandas',
-        'netCDF4'
+        'geopandas~=1.1.1',
+        'pandas~=2.3.1',
+        'netCDF4~=1.7.2',
+        'pydantic~=2.11.7',
+        'PyYAML~=6.0.2',
+        'setuptools~=5.6.0'
     ],
 )
-
