18253a93877d8c27b0fa5a1d0ccd5b6c3e8a395c
jeff.wade <jeff.s.wade@rtx.com>
2025-08-22 10:28:15 -0700

    Fixing typo in data provider

--- src/mswm/utils/ginputfunc.py
+++ src/mswm/utils/ginputfunc.py
@@ -2782,7 +2782,7 @@ def create_reg_realization_file(
         # Update forcing file path for catchment
         forcing_map = {
             "csv": {"forcing": {"file_pattern": ".*{{id}}.*.csv", "path": forcing_dir, "provider": "CsvPerFeature"}},
-            "bmi": {"forcing": {"path": forcing_dir, "provider": "ForcingsEngineLumpedDataProvide", "params": {"init_config": str(forcing_config_file)}}}
+            "bmi": {"forcing": {"path": forcing_dir, "provider": "ForcingsEngineLumpedDataProvider", "params": {"init_config": str(forcing_config_file)}}}
         }
 
         catmain["global"]["forcing"] = forcing_map[forcing_provider]
@@ -3161,7 +3161,7 @@ def create_realization_file(
     # Set forcing configuration
     forcing_map = {
         "csv": {"forcing": {"file_pattern": ".*{{id}}.*.csv", "path": forcing_dir, "provider": "CsvPerFeature"}},
-        "bmi": {"forcing": {"path": forcing_dir, "provider": "ForcingsEngineLumpedDataProvide", "params": {"init_config": str(forcing_config_file)}}}
+        "bmi": {"forcing": {"path": forcing_dir, "provider": "ForcingsEngineLumpedDataProvider", "params": {"init_config": str(forcing_config_file)}}}
     }
 
     g["global"]["forcing"] = forcing_map[forcing_provider]
