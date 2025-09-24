7a127b1b2cc625be8ae41fb90a769b0cc36182b5
jeff.wade <jeff.s.wade@rtx.com>
2025-09-24 11:58:52 -0700

    Added improved handling of forecast configuration options in pydantic

--- src/mswm/utils/input_configuration.py
+++ src/mswm/utils/input_configuration.py
@@ -156,16 +156,19 @@ class CalibConfig(StrictBaseModel):
         return self
 
 
+valid_configs = ['standard_ana', 'aorc', 'extended_ana', 'long_range_mem1', 'long_range_mem2', 'long_range_mem3', 'long_range_mem4',
+                 'medium_range_blend', 'nwm', 'short_range', 'short_range_alaska', 'medium_range_blend_alaska', 'short_range_extended_alaska',
+                 'short_range_hawaii', 'short_range_puertorico', 'extended_ana_alaska', 'standard_ana_alaska', 'standard_ana_hawaii',
+                 'standard_ana_puertorico']
+
+
 class ForcingConfig(StrictBaseModel):
     """
     Input.config Forcing section requirement
     """
     forcing_provider: Literal['csv', 'bmi']
     forcing_dir: Optional[str] = None
-    forecast_configuration: Optional[Literal['standard_ana', 'aorc', 'extended_ana', 'long_range_mem1', 'long_range_mem2', 'long_range_mem3', 'long_range_mem4',
-                                             'medium_range_blend', 'nwm', 'short_range', 'short_range_alaska', 'medium_range_blend_alaska', 'short_range_extended_alaska',
-                                             'short_range_hawaii', 'short_range_puertorico', 'extended_ana_alaska', 'standard_ana_alaska', 'standard_ana_hawaii',
-                                             'standard_ana_puertorico']] = None
+    forecast_configuration: Optional[str] = None
     cycle_datetime: Optional[str] = None
     forcing_template_dir: Optional[str] = None
     use_cold_start: Optional[bool] = None
@@ -181,7 +184,8 @@ class ForcingConfig(StrictBaseModel):
 
         # forecast_configuration required if forcing_provider is csv
         if self.forcing_provider == 'bmi' and self.forecast_configuration is None:
-            raise ValueError("`forecast_configuration` must be specified for a run using bmi forcing provider.")
+            raise ValueError("Invalid value for `forecast_configuration` for bmi forcing provider. "
+                             f"Valid options are: {', '.join(valid_configs)}.")
 
         # forcing dir required if forcing_provider is csv
         if self.forcing_provider == 'bmi' and self.forcing_template_dir is None:
