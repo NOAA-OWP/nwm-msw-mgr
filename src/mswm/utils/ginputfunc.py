f10c6273b569eaae514e5431fb6b77ad3b7390f2
jeff.wade <jeff.s.wade@rtx.com>
2026-04-21 09:42:25 -0400

    Update LASAM max soil types to 16

--- src/mswm/utils/ginputfunc.py
+++ src/mswm/utils/ginputfunc.py
@@ -1485,7 +1485,7 @@ def create_lasam_input(
                  'ponded_depth_max=1.1[cm]',
                  'use_closed_form_G=false',
                  'layer_soil_type=',
-                 'max_soil_types=15',
+                 'max_soil_types=16',
                  'wilting_point_psi=15495.0[cm]',
                  'field_capacity_psi=340.9[cm]',
                  'giuh_ordinates=0.06,0.51,0.28,0.12,0.03',
@@ -1507,9 +1507,16 @@ def create_lasam_input(
         if run_type == 'regionalization':
             mods = modules[i]
 
-        # Insert soil type
+        # Copy static config
         lasam_lst_catID = lasam_lst.copy()
-        lasam_lst_catID[9] = lasam_lst_catID[9] + str(int(dfa.loc[catID]['mode.ISLTYP']))
+
+        # Insert soil type, checking that it doesn't exceed max_soil_types=16
+        isltyp = int(dfa.loc[catID]['mode.ISLTYP'])
+        if isltyp > 16:
+            msg = f"LASAM soil type value {isltyp} exceeds max_soil_types=16"
+            logger.critical(msg)
+            raise ValueError(msg)
+        lasam_lst_catID[9] = lasam_lst_catID[9] + str(isltyp)
 
         # Check if sft is in use
         sft_coupled_str = 'true' if 'sft' in mods else 'false'
